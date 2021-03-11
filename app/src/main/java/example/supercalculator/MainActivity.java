16c350e53507719ab62e6d68a78f43cc3a7ae060
Carou5el <garrydeepcool@outlook.com>
2021-03-11 17:52:34 +0500

    Realize interface View.OnClickListener within MainActivity

--- app/src/main/java/example/supercalculator/MainActivity.java
+++ app/src/main/java/example/supercalculator/MainActivity.java
@@ -15,14 +15,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState == null)  {
-            logLifeCycle("First start.");
-        }
-        else    {
-            logLifeCycle("Another start.");
-        }
+//        if(savedInstanceState == null)  {
+//            logLifeCycle("First start.");
+//        }
+//        else    {
+//            logLifeCycle("Another start.");
+//        }
 
-        Button button8 = findViewById(R.id.button8);
+//        Button button8 = findViewById(R.id.button8);
 //        button8.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -36,49 +36,103 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        logLifeCycle("onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        logLifeCycle("onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        logLifeCycle("onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        logLifeCycle("onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        logLifeCycle("onDestroy");
-    }
-
-    // For example.
-    private void logLifeCycle(String log)   {
-        Toast.makeText(this, log, Toast.LENGTH_SHORT).show();
-        Log.d("MainActivity", log);
-
-    }
-
-
-
-
-
+    /**
+     * Realize interface View.OnClickListener.
+     * @param view - View object.
+     */
     @Override
     public void onClick(View view) {
+        switch (view.getId())   {
+            case R.id.button0:
+                //todo: make a method for this button.
+                break;
+            case R.id.button1:
+                //todo: make a method for this button.
+                break;
+            case R.id.button2:
+                //todo: make a method for this button.
+                break;
+            case R.id.button3:
+                //todo: make a method for this button.
+                break;
+            case R.id.button4:
+                //todo: make a method for this button.
+                break;
+            case R.id.button5:
+                //todo: make a method for this button.
+                break;
+            case R.id.button6:
+                //todo: make a method for this button.
+                break;
+            case R.id.button7:
+                //todo: make a method for this button.
+                break;
+            case R.id.button8:
+                //todo: make a method for this button.
+                break;
+            case R.id.button9:
+                //todo: make a method for this button.
+                break;
+            case R.id.buttonDot:
+                //todo: make a method for this button.
+                break;
+            case R.id.buttonDivision:
+                //todo: make a method for this button.
+                break;
+            case R.id.buttonMultiply:
+                //todo: make a method for this button.
+                break;
+            case R.id.buttonPlus:
+                //todo: make a method for this button.
+                break;
+            case R.id.buttonMinus:
+                //todo: make a method for this button.
+                break;
+            case R.id.buttonEqual:
+                //todo: make a method for this button.
+                break;
+
+        }
 
     }
+
+//    /**
+//     * Method for logging of application current status (onCreate, onResume, onStart,...).
+//     * Also push a toast notification.
+//     * @param log - string to push in toast.
+//     */
+//    private void logLifeCycle(String log)   {
+//        Toast.makeText(this, log, Toast.LENGTH_SHORT).show();
+//        Log.d("MainActivity", log);
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        logLifeCycle("onStart");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        logLifeCycle("onResume");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        logLifeCycle("onPause");
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        logLifeCycle("onStop");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        logLifeCycle("onDestroy");
+//    }
 }
