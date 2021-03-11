481a10f7a7db58ba1575387e3a11d7ee9f06a8ae
Carou5el <garrydeepcool@outlook.com>
2021-03-11 14:48:03 +0500

    Add a methods to all buttons.

--- app/src/main/java/example/supercalculator/MainActivity.java
+++ app/src/main/java/example/supercalculator/MainActivity.java
@@ -3,6 +3,7 @@ package example.supercalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick1(View view)  {
+
+    }
+    public void onClick2(View view)  {
+
+    }
+    public void onClick3(View view)  {
+
+    }
+    public void onClick4(View view)  {
+
+    }
+    public void onClick5(View view)  {
+
+    }
+    public void onClick6(View view)  {
+
+    }
+    public void onClick7(View view)  {
+
+    }
+    public void onClick8(View view)  {
+
+    }
+    public void onClick9(View view)  {
+
+    }
+    public void onClick0(View view)  {
+
+    }
+    public void onClickDot(View view)  {
+
+    }
+    public void onClickDivision(View view)  {
+
+    }
+    public void onClickMultiply(View view)  {
+
+    }
+    public void onClickPlus(View view)  {
+
+    }
+    public void onClickMinus(View view)  {
+
+    }
+    public void onClickEqual(View view)  {
+
     }
 }
